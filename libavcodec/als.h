9ae6cf645780df304c2919b64923f101fca8d4d3
Thilo Borgmann <Thilo@Thilo-Borgmanns-MacBook-Pro.local>
2010-01-15 02:40:56 +0100

    Introduce a common header for als encoder and decoder. Add a file for the encoder and include it into the framework. The encoder contains a skeleton able to write files containing zeros.

---
+++ libavcodec/als.h
@@ -0,0 +1,66 @@
+/*
+ * MPEG-4 ALS common header
+ * Copyright (c) 2010 Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ * Copyright (c) 2010 Justin Ruggles <justin.ruggles _at_ gmail.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file libavcodec/als.h
+ * MPEG-4 ALS common header
+ * @author Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ * @author Justin Ruggles <justin.ruggles _at_ gmail.com>
+ */
+
+
+#include <stdint.h>
+
+
+enum RA_Flag {
+    RA_FLAG_NONE,
+    RA_FLAG_FRAMES,
+    RA_FLAG_HEADER
+};
+
+
+typedef struct {
+    uint32_t samples;         ///< number of samples, 0xFFFFFFFF if unknown
+    int resolution;           ///< 000 = 8-bit; 001 = 16-bit; 010 = 24-bit; 011 = 32-bit
+    int floating;             ///< 1 = IEEE 32-bit floating-point, 0 = integer
+    int frame_length;         ///< frame length for each frame (last frame may differ)
+    int ra_distance;          ///< distance between RA frames (in frames, 0...255)
+    enum RA_Flag ra_flag;     ///< indicates where the size of ra units is stored
+    int adapt_order;          ///< adaptive order: 1 = on, 0 = off
+    int coef_table;           ///< table index of Rice code parameters
+    int long_term_prediction; ///< long term prediction (LTP): 1 = on, 0 = off
+    int max_order;            ///< maximum prediction order (0..1023)
+    int block_switching;      ///< number of block switching levels
+    int bgmc;                 ///< "Block Gilbert-Moore Code": 1 = on, 0 = off (Rice coding only)
+    int sb_part;              ///< sub-block partition
+    int joint_stereo;         ///< joint stereo: 1 = on, 0 = off
+    int mc_coding;            ///< extended inter-channel coding (multi channel coding): 1 = on, 0 = off
+    int chan_config;          ///< indicates that a chan_config_info field is present
+    int chan_sort;            ///< channel rearrangement: 1 = on, 0 = off
+    int rlslms;               ///< use "Recursive Least Square-Least Mean Square" predictor: 1 = on, 0 = off
+    int chan_config_info;     ///< mapping of channels to loudspeaker locations. Unused until setting channel configuration is implemented.
+    int *chan_pos;            ///< original channel positions
+    uint32_t header_size;     ///< header size of original audio file in bytes, provided for debugging
+    uint32_t trailer_size;    ///< trailer size of original audio file in bytes, provided for debugging
+} ALSSpecificConfig;
+
+
---
+++ libavcodec/alsenc.c
@@ -0,0 +1,288 @@
+/*
+ * MPEG-4 ALS encoder
+ * Copyright (c) 2010 Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ * Copyright (c) 2010 Justin Ruggles <justin.ruggles _at_ gmail.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/**
+ * @file libavcodec/alsenc.c
+ * MPEG-4 ALS encoder
+ * @author Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ * @author Justin Ruggles <justin.ruggles _at_ gmail.com>
+ */
+
+
+#define DEBUG
+
+
+#include "als.h"
+
+#include "avcodec.h"
+#include "put_bits.h"
+#include "mpeg4audio.h"
+
+
+#define ALS_SPECIFIC_CFG_SIZE  30
+#define ALS_EXTRADATA_MAX_SIZE (6 + ALS_SPECIFIC_CFG_SIZE)
+
+
+typedef struct {
+    AVCodecContext *avctx;
+    ALSSpecificConfig sconf;
+    PutBitContext pb;
+} ALSEncContext;
+
+
+static av_cold int get_specific_config(AVCodecContext *avctx)
+{
+    ALSEncContext *ctx       = avctx->priv_data;
+    ALSSpecificConfig *sconf = &ctx->sconf;
+
+
+    // don't know the total number of samples(?)
+    sconf->samples = 0xFFFFFFFF;
+
+
+    // determine sample format
+    if        (avctx->sample_fmt == SAMPLE_FMT_U8) {
+        sconf->resolution = 0;
+    } else if (avctx->sample_fmt == SAMPLE_FMT_S16) {
+        sconf->resolution = 1;
+    } else if (avctx->sample_fmt == SAMPLE_FMT_S32) {
+        sconf->resolution = 3;
+    } else if (avctx->sample_fmt == SAMPLE_FMT_FLT) {
+        sconf->resolution = 3;
+        sconf->floating   = 1;
+        av_log(avctx, AV_LOG_ERROR, "floating-point samples are not supported\n");
+    } else {
+        av_log(avctx, AV_LOG_ERROR, "unsupported sample format\n");
+        return -1;
+    }
+
+    avctx->bits_per_raw_sample = (sconf->resolution + 1) << 3;
+
+
+    // determine frame length
+    if (avctx->frame_size <= 0)
+        avctx->frame_size = 2048;
+
+    sconf->frame_length = avctx->frame_size;
+
+
+    // determine distance between ra-frames. 0 = no ra, 1 = all ra
+    sconf->ra_distance = 0;
+
+
+    // determine where to store ra_flag (01: beginning of frame_data)
+    sconf->ra_flag = RA_FLAG_FRAMES;
+
+
+    // determine if adaptive prediction order is used
+    sconf->adapt_order = 0;
+
+
+    // determine the coef_table to be used (no samples: not yet possible?)
+    sconf->coef_table = 0;
+
+
+    // determine if long-term prediction is used (user defined?)
+    sconf->long_term_prediction = 0;
+
+
+    // determine a maximum prediction order (no samples: not yet possible?)
+    sconf->max_order = 10;
+
+
+    // determine if block-switching is used (user defined, no samples: ...?)
+    sconf->block_switching = 0;
+
+
+    // determine if BGMC mode is used (user defined)
+    sconf->bgmc = 0;
+
+
+    // determine what sub-block partitioning is used
+    sconf->sb_part = 0;
+
+
+    // determine if joint-stereo is used (user defined, standard = yes?)
+    sconf->joint_stereo = 0; // = 0 for simplicity now
+
+
+    // determine if multi-channel coding is used (user defined && sanity check: channels > 2 [although specs allow mc_coding for 2 channels... maybe give a warning])
+    sconf->mc_coding = 0;
+
+
+    // determine manual channel configuration (user defined?)
+    sconf->chan_config = 0;
+    sconf->chan_config_info = 0;
+
+
+    // determine channel sorting (user defined)
+    sconf->chan_sort = 0;
+    sconf->chan_pos  = NULL;
+
+
+    // determine if backward adaptive is used (user defined)
+    sconf->rlslms = 0;
+
+
+    // determine size of original header/trailer
+    // (possible in ffmpeg? at all useful?)
+    sconf->header_size  = 0;
+    sconf->trailer_size = 0;
+
+
+    return 0;
+}
+
+
+static int write_specific_config(AVCodecContext *avctx)
+{
+    ALSEncContext *ctx       = avctx->priv_data;
+    ALSSpecificConfig *sconf = &ctx->sconf;
+    PutBitContext pb;
+
+    uint8_t header[ALS_EXTRADATA_MAX_SIZE + FF_INPUT_BUFFER_PADDING_SIZE];
+
+    memset(header, 0, sizeof(header));
+    init_put_bits(&pb, header, ALS_EXTRADATA_MAX_SIZE);
+
+
+    // AudioSpecificConfig, reference to ISO/IEC 14496-3 section 1.6.2.1 & 1.6.3
+
+    // object type (GetAudioObjectType(), 14496-3 1.6.2.1.1 Table 1.16)
+    put_bits(&pb, 5, AOT_ESCAPE);
+    put_bits(&pb, 6, AOT_ALS - 32);
+
+    // samplingFrequencyIndex
+    put_bits(&pb,  4, 0xF);
+    put_bits(&pb, 24, avctx->sample_rate); // samplingFrequency
+
+    // channelConfiguraiton
+    put_bits(&pb, 4, 0); // 0 = defined in DecoderSpecificConfig
+
+
+    // switch(AudioObjectType) -> case 36: ALSSpecificConfig
+
+    // fillBits (align)
+    align_put_bits(&pb);
+
+    put_bits32(&pb,     MKBETAG('A', 'L', 'S', '\0'));
+    put_bits32(&pb,     avctx->sample_rate);
+    put_bits32(&pb,     sconf->samples);
+    put_bits  (&pb, 16, avctx->channels - 1);
+    put_bits  (&pb,  3, 0);                      // original file_type (0 = unknown)
+    put_bits  (&pb,  3, sconf->resolution);
+    put_bits  (&pb,  1, sconf->floating);
+    put_bits  (&pb,  1, 0);                      // msb first
+    put_bits  (&pb, 16, sconf->frame_length - 1);
+    put_bits  (&pb,  8, sconf->ra_distance);
+    put_bits  (&pb,  2, sconf->ra_flag);
+    put_bits  (&pb,  1, sconf->adapt_order);
+    put_bits  (&pb,  2, sconf->coef_table);
+    put_bits  (&pb,  1, sconf->long_term_prediction);
+    put_bits  (&pb, 10, sconf->max_order);
+    put_bits  (&pb,  2, sconf->block_switching);
+    put_bits  (&pb,  1, sconf->bgmc);
+    put_bits  (&pb,  1, sconf->sb_part);
+    put_bits  (&pb,  1, sconf->joint_stereo);
+    put_bits  (&pb,  1, sconf->mc_coding);
+    put_bits  (&pb,  1, sconf->chan_config);
+    put_bits  (&pb,  1, sconf->chan_sort);
+    put_bits  (&pb,  1, 0);                     // crc_enabled (0 = none, TODO! add to sconf!)
+    put_bits  (&pb,  1, sconf->rlslms);
+    put_bits  (&pb,  5, 0);                     // reserved bits
+    put_bits  (&pb,  1, 0);                     // aux_data_enabled (0 = false)
+
+    // align
+    align_put_bits(&pb);
+
+    put_bits32(&pb,     sconf->header_size);
+    put_bits32(&pb,     sconf->trailer_size);
+
+
+    // writing in local header finished, now copy that to extradata
+    if (avctx->extradata)
+        av_freep(avctx->extradata);
+
+    avctx->extradata_size = put_bits_count(&pb) >> 3;
+
+    avctx->extradata = av_mallocz(avctx->extradata_size +
+                                  FF_INPUT_BUFFER_PADDING_SIZE);
+    if (!avctx->extradata)
+        return AVERROR(ENOMEM);
+
+    memcpy(avctx->extradata, header, avctx->extradata_size);
+
+    return 0;
+}
+
+
+static av_cold int encode_init(AVCodecContext *avctx)
+{
+    ALSEncContext *ctx = avctx->priv_data;
+    int ret;
+
+    ctx->avctx = avctx;
+
+    // determine ALSSpecificConfig
+    if (get_specific_config(avctx))
+        return -1;
+
+    // write AudioSpecificConfig & ALSSpecificConfig
+    ret = write_specific_config(avctx);
+    if (ret)
+        return ret;
+
+    return 0;
+}
+
+
+static int encode_frame(AVCodecContext *avctx, uint8_t *frame,
+                        int buf_size, void *data)
+{
+    ALSEncContext *ctx       = avctx->priv_data;
+    ALSSpecificConfig *sconf = &ctx->sconf;
+
+    memset(frame, 0, buf_size);
+    return (avctx->bits_per_raw_sample >> 3) *
+           avctx->channels *
+           sconf->frame_length; // to be replaced by #samples actually written
+}
+
+
+static av_cold int encode_end(AVCodecContext *avctx)
+{
+    return 0;
+}
+
+
+AVCodec als_encoder = {
+    "als",
+    CODEC_TYPE_AUDIO,
+    CODEC_ID_MP4ALS,
+    sizeof(ALSEncContext),
+    encode_init,
+    encode_frame,
+    encode_end,
+    NULL,
+    .capabilities = CODEC_CAP_SMALL_LAST_FRAME,         // CAP_DELAY?
+    .long_name = NULL_IF_CONFIG_SMALL("MPEG-4 Audio Lossless Coding (ALS)"),
+};
