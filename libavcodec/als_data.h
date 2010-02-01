37012476cc08f12535a92f05d1ca38089c86de0a
Justin Ruggles <justin.ruggles@gmail.com>
2010-01-31 21:57:52 -0500

    Move some shared tables to als_data.c/h

---
+++ libavcodec/als_data.h
@@ -0,0 +1,31 @@
+/*
+ * ALS encoder and decoder tables
+ * Copyright (c) 2010 Thilo Borgmann <thilo.borgmann _at_ googlemail.com>
+ * Copyright (c) 2010 Justin Ruggles <justin.ruggles@gmail.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVCODEC_ALS_DATA_H
+#define AVCODEC_ALS_DATA_H
+
+#include <stdint.h>
+
+extern const int16_t ff_als_parcor_scaled_values[128];
+extern const int8_t  ff_als_parcor_rice_table[3][20][2];
+
+#endif /* AVCODEC_ALS_DATA_H */
