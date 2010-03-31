3ed084750a48fe94144154616de5d8d23cdf0d63
Justin Ruggles <justin.ruggles@gmail.com>
2010-03-31 19:01:14 -0400

    6% faster put_bits_follow()

--- libavcodec/bgmc.c
+++ libavcodec/bgmc.c
@@ -607,9 +607,10 @@ void ff_bgmc_decode(GetBitContext *gb, unsigned int num, int32_t *dst,
  */
 static void put_bits_follow(PutBitContext *pb, unsigned int bit, unsigned int *follow)
 {
-    put_bits (pb, 1, bit);
-    for (unsigned int i = 0; i < *follow; i++)
-        put_bits(pb, 1, !bit);
+    if (bit)
+        put_bits(pb, 1 + *follow,  1 << *follow     );
+    else
+        put_bits(pb, 1 + *follow, (1 << *follow) - 1);
 
     *follow = 0;
 }
