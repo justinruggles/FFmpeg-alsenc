927151521a0811e295ffb98a8b286cb1fd8e820e
Justin Ruggles <justin.ruggles@gmail.com>
2010-08-21 16:21:12 -0400

    Fix storing of previous samples

--- libavcodec/alsenc.c
+++ libavcodec/alsenc.c
@@ -2938,13 +2938,11 @@ static int encode_frame(AVCodecContext *avctx, uint8_t *frame,
         sconf->samples += avctx->frame_size;
 
     // store previous samples
-    if (ctx->ra_counter || !sconf->ra_distance) {
         for (c = 0; c < avctx->channels; c++) {
             memcpy(ctx->raw_samples[c] - sconf->max_order,
                    ctx->raw_samples[c] + avctx->frame_size - sconf->max_order,
                    sizeof(*ctx->raw_samples[c]) * sconf->max_order);
         }
-    }
 
     return frame_data_size;
 }
